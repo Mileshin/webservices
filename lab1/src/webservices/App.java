0140fb5921a5ddcb64214d1ca9a2476a013cbdad
Àíäðåé Ìèëåøèí <andrej.mileshin@yandex.ru>
2020-02-26 19:44:13 +0300

    Add select

---
+++ lab1/src/webservices/App.java
@@ -0,0 +1,12 @@
+package webservices;
+
+import javax.xml.ws.Endpoint;
+
+public class App {
+
+    public static void main(String[] args) {
+        String url = "http://0.0.0.0:2987/PersonService";
+        Endpoint.publish(url, new PersonWebService());
+        System.out.println("Started on:" + url);
+    }
+}
