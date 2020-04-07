8d4d5ff47a020fdced0e9ea40184746383005921
Andrey Mileshin <andrej.mileshin@yandex.ru>
2020-04-08 00:17:19 +0300

    Added lab7

---
+++ lab7/src/webservices/App.java
@@ -0,0 +1,39 @@
+package webservices;
+
+public class App {
+    private final static String publishMethodName = "publish";
+    private final static String browseMethodName = "browse";
+
+    public static void main(String[] args) {
+        String method = (args.length > 0) ? args[0] : "default";
+
+        switch (method) {
+            case publishMethodName: {
+                checkParamsLength(args, 4);
+                new Publisher().publish(args[1].trim(), args[2].trim(), args[3].trim());
+                break;
+            }
+            case browseMethodName: {
+                checkParamsLength(args, 1);
+                new Browser().Browse();
+                break;
+            }
+            default: {
+                String operations = String.join(", ", publishMethodName, browseMethodName);
+                System.err.println(
+                        String.format("Program requires first cli argument to be name of operation: %s.", operations)
+                );
+                System.exit(1);
+                break;
+            }
+        }
+    }
+
+    private static void checkParamsLength(String[] args, int length) {
+        if (args.length < length) {
+            String message = String.format("Wrong number of parameters, got: %d, expected: %d", args.length, length);
+            System.err.println(message);
+            System.exit(1);
+        }
+    }
+}
